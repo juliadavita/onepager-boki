4f1542d2a74045a221fe6822555354df8f7f914e
Julia Lanu <julia@erecode.nl>
2021-03-26 11:58:29 +0100

    Start project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { TeamComponent } from './components/team/team.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroComponent,
+    TeamComponent
   ],
   imports: [
     BrowserModule
